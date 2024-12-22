8efeb57380cfa84ea0993cd58911bd8387169ceb
ruitao-zhang <3210105252@zju.edu.cn>
2024-12-22 19:38:11 +0800

    Update main.cpp

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -1,1 +1,16 @@
+#include
+int main() {
+    string expression;
+    cout << "请输入一个数学表达式: ";
+    getline(cin, expression);  // 获取包含空格的输入3b
+
+    try {
+        double result = evaluate(expression);
+        cout << "结果: " << result << endl;
+    } catch (const runtime_error& e) {
+        cout << "ILLEGAL" << endl;
+    }
+
+    return 0;
+}
 
